cf6f6d81f0ee265b6a75ea48cd56eebf8aea85fc
ioankont <118340733+ioankont@users.noreply.github.com>
2023-10-03 10:11:30 +0300

    Create configs.py

---
+++ Saliency_Detection/ATSal/attention/configs.py
@@ -0,0 +1,42 @@
+import argparse
+
+def training_args():
+    parser = argparse.ArgumentParser(description='Description for args of train.py ATSal model')
+
+    # Model parameters
+    parser.add_argument('--gpu', type=str, default="cuda:0",
+                        help='Use of gpu, if not available chooses "cpu"')
+    parser.add_argument('--attention_model', type=str, default="attention/weights/initial.pt",
+                        help='Attention model weights path, Default attention/weights/initial.pt')
+
+    # Data loader parameters
+    parser.add_argument('--path_to_frames_folder', type=str,
+                        default=r'CA-SUM-360/data/Salient360-Sitzman/training/frames',
+                        help='Path to the folder with the extracted ERP frames')
+    parser.add_argument('--process', type=str,
+                        default='train',
+                        help='Process for data loader for train.py')
+    parser.add_argument('--resolution', type=int, nargs=2, default=[320, 640],
+                        help='Resolution of the ERP images for the model')
+    parser.add_argument('--clip_size', type=int, default=1, help='Frames per data for data loader')
+    parser.add_argument('--batch_size', type=int, default=1, help='Batch size for data loader')
+
+    #optimizer
+    parser.add_argument('--lr', type=float, default=1e-5, help='Learning Rate for ADAM optimizer')
+    parser.add_argument('--weight_decay', type=float, default=1e-4, help='Weight Decay for ADAM optimizer')
+
+    parser.add_argument('--epochs', type=int, default=90, help='90 epochs for train on Salient360-Sitzman dataset,10 for VR-EyeTracking')
+    # Output_path
+    parser.add_argument('--path_to_save_weights', type=str,
+                        default=r'CA-SUM-360/Saliency_Detection/ATSal/attention/weights',
+                        help='Path to save the checkpoint_weights')
+
+    return parser
+
+
+
+
+if __name__ == '__main__':
+
+   args = training_args()
+   print(args)
