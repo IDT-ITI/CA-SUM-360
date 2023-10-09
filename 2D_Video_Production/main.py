9e6bc812e89b9f8a584cb82dfd527c3a7427c1e3
ioankont <118340733+ioankont@users.noreply.github.com>
2023-10-09 12:06:29 +0300

    Update main.py

--- 2D_Video_Production/main.py
+++ 2D_Video_Production/main.py
@@ -14,36 +14,21 @@ def run(**kwargs):
     resolution = kwargs['resolution']
     saliency_maps_path = kwargs['saliency_maps_path']
     spatial_distance = kwargs['spatial_distance']
-    # a function to delete all files in a subfolder.
-    def delete_files_in_subfolder(subfolder_path):
-        for filename in os.listdir(subfolder_path):
-            file_path = os.path.join(subfolder_path, filename)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-
-
-    # Iterate through all subfolders in the main folder.
-    for subfolder_name in os.listdir(path_to_folder):
-        subfolder_path = os.path.join(path_to_folder, subfolder_name)
-
-        # Check if it's a directory (subfolder).
-        if os.path.isdir(subfolder_path):
-            # delete files in this subfolder.
-            delete_files_in_subfolder(subfolder_path)
+    
+   
     list_of_saliency_paths = os.listdir(saliency_maps_path)
     length = len(list_of_saliency_paths)
     salient_regions = []
-    for i in range(length-200):  # Replace  with the total number of frames
+    for i in range(length):  # Replace  with the total number of frames
 
 
-        saliency_map = cv2.imread(saliency_maps_path + "/" + f"{i+21:04d}.png", cv2.IMREAD_GRAYSCALE)
-        saliency_map = cv2.resize(saliency_map, (resolution[1], resolution[0]))
+        saliency_map = cv2.imread(saliency_maps_path + "/" + f"{i:04d}.png", cv2.IMREAD_GRAYSCALE)
+        saliency_map = cv2.resize(saliency_map, (resolution[1], resolution[0]),interpolation=cv2.INTER_AREA)
+
 
 
         bounding_boxes,salient_scores = extract_salient_regions(saliency_map,intensity_value,dbscan_distance,resolution=[resolution[0],resolution[1]])
-
-
-        salient_regions.append((f"{i+21:04d}", bounding_boxes,salient_scores))
+        salient_regions.append((f"{i:04d}", bounding_boxes,salient_scores))
 
     frame_number = [item[0] for item in salient_regions]
     # Example usage:
