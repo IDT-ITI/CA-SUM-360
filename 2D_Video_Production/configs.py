f3f9d68736ed4f9beeeee6c245ec1de5ff4eb651
ioankont <118340733+ioankont@users.noreply.github.com>
2023-10-09 12:09:26 +0300

    Update configs.py

--- 2D_Video_Production/configs.py
+++ 2D_Video_Production/configs.py
@@ -1,27 +1,25 @@
 import argparse
 
 def args():
-    parser = argparse.ArgumentParser(description='Args for the 2D video production task')
+    parser = argparse.ArgumentParser(description='Your script description here.')
     parser.add_argument('--saliency_maps_path', type=str,
-                        default=r"D:\Program Files\IoanProjects\StaticVRval\saliency1\180",
+                        default=r"D:\Program Files\IoanProjects\summary_data\saliency_sports",
                         help='Directory path of saliency maps')
-    parser.add_argument('--frames_path', type=str, default=r"D:\Program Files\IoanProjects\StaticVRval\frames1\180",
+    parser.add_argument('--frames_path', type=str, default=r"D:\Program Files\IoanProjects\360_Saliency_dataset_2018ECCV",
                         help='Directory path of frames')
 
     parser.add_argument('--intensity_value', type=int, default=150, help='Intensity value parameter')
-    parser.add_argument('--dbscan_distance', type=float, default=1.2, help='DBSCAN distance parameter')
-    parser.add_argument('--spatial_distance', type=int, default=100, help='Spatial distance parameter')
-    parser.add_argument('--fill_loss', type=int, default=100, help='Fill loss parameter')
-
-
+    parser.add_argument('--dbscan_distance', type=float, default=600, help='DBSCAN distance parameter')
+    parser.add_argument('--spatial_distance', type=int, default=75, help='Spatial distance parameter')
+    parser.add_argument('--fill_loss', type=int, default=25, help='Fill loss parameter')
 
     parser.add_argument('--path_to_folder', type=str,
-                        default=r'C:\Users\ioankont\PycharmProjects\SalientRegionExtractor\summaryFolder',
+                        default=r'CA-SUM360/2D_Video_Production/outputs_fovs',
                         help='Path to save subvideos frames')
-    parser.add_argument('--resolution', type=int, nargs=2, default=[1024, 2048], help='Resolution parameter')
-  
+    parser.add_argument('--resolution', type=int, nargs=2, default=[1080, 1920], help='Resolution parameter')
+    # Your code here
     return parser
 if __name__ == '__main__':
+
     args = args()
-    print(args)
-   
+    print(args.parse_args())
