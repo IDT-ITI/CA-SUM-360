ba8cf98682636d341d33d8882aaf4375654cbea4
ioankont <118340733+ioankont@users.noreply.github.com>
2023-10-10 15:59:00 +0300

    Update inference.py

--- Saliency_Detection/SST-Sal/inference.py
+++ Saliency_Detection/SST-Sal/inference.py
@@ -10,7 +10,12 @@ from metrics import Metrics
 
 from configs import inference_args
 
+current_script_path = os.path.abspath(__file__)
 
+# Navigate to the parent directory (one level up)
+parent_directory = os.path.dirname(current_script_path)
+grant_parent_directory = os.path.dirname(parent_directory)
+grant_parent_directory = os.path.dirname(grant_parent_directory)
 
 
 
@@ -27,6 +32,7 @@ def test(test_data, model ,device,output_path):
     with torch.no_grad():
         count = 0
         for j,video in enumerate(test_data):
+
             cc_metric = []
             sim_metric = []
 
@@ -84,18 +90,23 @@ if __name__ == "__main__":
     resolution = args.resolution
     clip_size = args.clip_size
     batch_size = args.batch_size
-    save_outputs_path = args.path_to_save_saliency_maps
+    path_to_save_saliency_maps = args.path_to_save_saliency_maps
     device = torch.device(gpu if torch.cuda.is_available() else "cpu")
-
+    print(sst_sal)
     model = torch.load(sst_sal, map_location=device)
-    with open('CA-SUM-360/data/Static-VR-EyeTracking/val_split.txt', 'r') as file:
+    val_path_txt = os.path.join(grant_parent_directory,"data/Static-VR-EyeTracking/val_split.txt")
+    with open(val_path_txt, 'r') as file:
         # Read the content of the file and split it by commas
         content = file.read()
         videos = content.split(',')
     static_val_videos = [video.strip() for video in videos]
-    
+    path_to_frames_validation_folder = os.path.join(grant_parent_directory,path_to_frames_validation_folder)
+    if os.path.exists(path_to_save_saliency_maps):
+        print(path_to_save_saliency_maps+"exists")
+    else:
+        os.mkdir(path_to_save_saliency_maps)
     test_video_dataset = RGB(path_to_frames_validation_folder,static_val_videos,process=process,frames_per_data=clip_size,resolution=resolution)
     test_data = DataLoader(test_video_dataset, batch_size=batch_size, drop_last=True)
 
 
-    test(test_data, model, device,save_outputs_path)
+    test(test_data, model, device,path_to_save_saliency_maps)
