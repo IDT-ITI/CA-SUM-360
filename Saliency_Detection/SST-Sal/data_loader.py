442e58b0fed67bcecb9568a1484885a3b5b17628
ioankont <118340733+ioankont@users.noreply.github.com>
2023-10-11 11:34:18 +0300

    Update data_loader.py

--- Saliency_Detection/SST-Sal/data_loader.py
+++ Saliency_Detection/SST-Sal/data_loader.py
@@ -7,13 +7,14 @@ import numpy as np
 
 
 class RGB(Dataset):
-    def __init__(self, path_to_frames,static_videos, process,frames_per_data,resolution=[240, 320]):
+    def __init__(self, path_to_frames,static_videos,load_gt, process,frames_per_data,resolution=[240, 320]):
         self.path_to_frames = path_to_frames
         self.video_names = static_videos
         self.resolution = resolution
         self.dataset = []
         self.frames_per_data = frames_per_data
         self.process = process
+        self.load_gt = load_gt
         #video_names = os.listdir(self.path_to_frames)
         if self.process=="train":
             for i, file in enumerate(self.video_names):
@@ -40,10 +41,10 @@ class RGB(Dataset):
                         fpd = fpd+ self.frames_per_data
 
         else:
+
             for i, file in enumerate(self.video_names):
 
                 frame_folder = os.path.join(self.path_to_frames, file)
-                print(frame_folder)
                 if os.path.exists(frame_folder):
 
                     img_files = []
@@ -112,38 +113,65 @@ class RGB(Dataset):
 
             final.append((torch.cat(frame_img, 0), torch.cat(label, 0)))
         else:
-            for i, path_to_frames in enumerate(frames):
-                frame_img = []
-                label = []
+            if self.load_gt == "True":
+                for i, path_to_frames in enumerate(frames):
+                    frame_img = []
+                    label = []
 
-                for path_to_frame in path_to_frames:
+                    for path_to_frame in path_to_frames:
 
-                    path_to_gt = path_to_frame.replace('frames', 'saliency')
+                        path_to_gt = path_to_frame.replace('frames', 'saliency')
 
-                    img_frame = cv2.imread(path_to_frame)
+                        img_frame = cv2.imread(path_to_frame)
 
-                    if img_frame.shape[1] != self.resolution[1] or img_frame.shape[0] != self.resolution[0]:
-                        img_frame = cv2.resize(img_frame, (self.resolution[1], self.resolution[0]),
-                                               interpolation=cv2.INTER_AREA)
-                    img_frame = img_frame.astype(np.float32)
+                        if img_frame.shape[1] != self.resolution[1] or img_frame.shape[0] != self.resolution[0]:
+                            img_frame = cv2.resize(img_frame, (self.resolution[1], self.resolution[0]),
+                                                   interpolation=cv2.INTER_AREA)
+                        img_frame = img_frame.astype(np.float32)
 
-                    img_frame = img_frame / 255
+                        img_frame = img_frame / 255
 
-                    img_frame = torch.FloatTensor(img_frame)
-                    img_frame = img_frame.permute(2, 0, 1)
-                    img_frame = img_frame.unsqueeze(0)
-                    frame_img.append(img_frame)
+                        img_frame = torch.FloatTensor(img_frame)
+                        img_frame = img_frame.permute(2, 0, 1)
+                        img_frame = img_frame.unsqueeze(0)
+                        frame_img.append(img_frame)
 
-                    img_gt = cv2.imread(path_to_gt, cv2.IMREAD_GRAYSCALE)
-                    if img_gt.shape[1] != self.resolution[1] or img_gt.shape[0] != self.resolution[0]:
-                        img_gt = cv2.resize(img_gt, (self.resolution[1], self.resolution[0]),
-                                            interpolation=cv2.INTER_AREA)
-                    img_gt = img_gt.astype(np.float32)
-                    img_gt = (img_gt - np.min(img_gt)) / (np.max(img_gt) - np.min(img_gt))
-                    img_gt = torch.FloatTensor(img_gt)
-                    img_gt = img_gt.unsqueeze(0)
-                    label.append(img_gt.unsqueeze(0))
+                        img_gt = cv2.imread(path_to_gt, cv2.IMREAD_GRAYSCALE)
+                        if img_gt.shape[1] != self.resolution[1] or img_gt.shape[0] != self.resolution[0]:
+                            img_gt = cv2.resize(img_gt, (self.resolution[1], self.resolution[0]),
+                                                interpolation=cv2.INTER_AREA)
+                        img_gt = img_gt.astype(np.float32)
+                        img_gt = (img_gt - np.min(img_gt)) / (np.max(img_gt) - np.min(img_gt))
+                        img_gt = torch.FloatTensor(img_gt)
+                        img_gt = img_gt.unsqueeze(0)
+                        label.append(img_gt.unsqueeze(0))
 
-                final.append((torch.cat(frame_img, 0), torch.cat(label, 0)))
+                    final.append((torch.cat(frame_img, 0), torch.cat(label, 0)))
+            else:
+                for i, path_to_frames in enumerate(frames):
+                    frame_img = []
+                    label = []
+
+                    for path_to_frame in path_to_frames:
+
+
+
+                        img_frame = cv2.imread(path_to_frame)
+
+                        if img_frame.shape[1] != self.resolution[1] or img_frame.shape[0] != self.resolution[0]:
+                            img_frame = cv2.resize(img_frame, (self.resolution[1], self.resolution[0]),
+                                                   interpolation=cv2.INTER_AREA)
+                        img_frame = img_frame.astype(np.float32)
+
+                        img_frame = img_frame / 255
+
+                        img_frame = torch.FloatTensor(img_frame)
+                        img_frame = img_frame.permute(2, 0, 1)
+                        img_frame = img_frame.unsqueeze(0)
+                        frame_img.append(img_frame)
+
+
+                    final.append(torch.cat(frame_img, 0))
+
 
         return final
