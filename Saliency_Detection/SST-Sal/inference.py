d5cd9d5833d6dfc30f24304afa14bc2ed81f243d
ioankont <118340733+ioankont@users.noreply.github.com>
2023-10-11 11:33:56 +0300

    Update inference.py

--- Saliency_Detection/SST-Sal/inference.py
+++ Saliency_Detection/SST-Sal/inference.py
@@ -20,7 +20,7 @@ grant_parent_directory = os.path.dirname(grant_parent_directory)
 
 
 
-def test(test_data, model ,device,output_path):
+def test(test_data, model,load_gt ,device,output_path):
 
 
     model.to(device)
@@ -30,53 +30,79 @@ def test(test_data, model ,device,output_path):
 
 
     with torch.no_grad():
-        count = 0
-        for j,video in enumerate(test_data):
+        if load_gt=="True":
 
-            cc_metric = []
-            sim_metric = []
+            for j,video in enumerate(test_data):
 
-            for k,(x,y) in enumerate(video):
+                cc_metric = []
+                sim_metric = []
 
-
-                pred = model(x.to(device))
-
-                batch_size, Nframes, _, _ = pred[:, :, 0, :, :].shape
-
-                if k ==0:
-
-                    for bs in range(0,batch_size):
-
-                        for i in range(0,Nframes):
-                            sal = np.array((pred[bs][i][0].cpu() - torch.min(pred[bs][i][0].cpu())) / (
-                                    torch.max(pred[bs][i][0].cpu()) - torch.min(pred[bs][i][0].cpu())))
-                            cv2.imwrite(os.path.join(output_path,f'{count:04d}.png'), (sal * 255).astype(np.uint8))
-                            count += 1
-                            cc,sim= Metrics(pred[bs][i][0].cpu(),y[bs][i][0].cpu())
-                            cc_metric.append(cc)
-                            sim_metric.append(sim)
+                for k,(x,y) in enumerate(video):
 
 
-                else:
-                    for bs in range(0, batch_size):
-                        for i in range(4, Nframes):
+                    pred = model(x.to(device))
 
-                            sal = np.array((pred[bs][i][0].cpu() - torch.min(pred[bs][i][0].cpu())) / (
+                    batch_size, Nframes, _, _ = pred[:, :, 0, :, :].shape
+
+                    if k ==0:
+
+                        for bs in range(0,batch_size):
+
+                            for i in range(0,Nframes):
+
+                                cc,sim= Metrics(pred[bs][i][0].cpu(),y[bs][i][0].cpu())
+                                cc_metric.append(cc)
+                                sim_metric.append(sim)
+
+
+                    else:
+                        for bs in range(0, batch_size):
+                            for i in range(4, Nframes):
+
+                                cc, sim = Metrics(pred[bs][i][0].cpu(), y[bs][i][0].cpu())
+                                cc_metric.append(cc)
+                                sim_metric.append(sim)
+
+                #print(np.mean(cc_metric))
+                #print(np.mean(sim_metric))
+
+                ccall.append(np.mean(cc_metric))
+                simall.append(np.mean(sim_metric))
+
+            print(np.mean(ccall))
+            print(np.mean(simall))
+
+        else:
+            count = 0
+            for j, video in enumerate(test_data):
+                for k, (x) in enumerate(video):
+
+                    pred = model(x.to(device))
+                    batch_size, Nframes, _, _ = pred[:, :, 0, :, :].shape
+
+                    if k == 0:
+
+                        for bs in range(0, batch_size):
+                            for i in range(0, Nframes):
+                                sal = np.array((pred[bs][i][0].cpu() - torch.min(pred[bs][i][0].cpu())) / (
                                         torch.max(pred[bs][i][0].cpu()) - torch.min(pred[bs][i][0].cpu())))
-                            cv2.imwrite(os.path.join(output_path, f'{count:04d}.png'), (sal * 255).astype(np.uint8))
-                            count += 1
-                            cc, sim = Metrics(pred[bs][i][0].cpu(), y[bs][i][0].cpu())
-                            cc_metric.append(cc)
-                            sim_metric.append(sim)
+                                cv2.imwrite(os.path.join(output_path, f'{count:04d}.png'), (sal * 255).astype(np.uint8))
+                                count += 1
 
-            #print(np.mean(cc_metric))
-            #print(np.mean(sim_metric))
 
-            ccall.append(np.mean(cc_metric))
-            simall.append(np.mean(sim_metric))
+                    else:
+                        for bs in range(0, batch_size):
+                            for i in range(4, Nframes):
+                                sal = np.array((pred[bs][i][0].cpu() - torch.min(pred[bs][i][0].cpu())) / (
+                                        torch.max(pred[bs][i][0].cpu()) - torch.min(pred[bs][i][0].cpu())))
+                                cv2.imwrite(os.path.join(output_path, f'{count:04d}.png'), (sal * 255).astype(np.uint8))
+                                count += 1
 
-    print(np.mean(ccall))
-    print(np.mean(simall))
+
+                # print(np.mean(cc_metric))
+                # print(np.mean(sim_metric))
+
+
 
 
 
@@ -91,8 +117,9 @@ if __name__ == "__main__":
     clip_size = args.clip_size
     batch_size = args.batch_size
     path_to_save_saliency_maps = args.path_to_save_saliency_maps
+    load_gt = args.load_gt
     device = torch.device(gpu if torch.cuda.is_available() else "cpu")
-    print(sst_sal)
+
     model = torch.load(sst_sal, map_location=device)
     val_path_txt = os.path.join(grant_parent_directory,"data/Static-VR-EyeTracking/val_split.txt")
     with open(val_path_txt, 'r') as file:
@@ -105,8 +132,8 @@ if __name__ == "__main__":
         print(path_to_save_saliency_maps+"exists")
     else:
         os.mkdir(path_to_save_saliency_maps)
-    test_video_dataset = RGB(path_to_frames_validation_folder,static_val_videos,process=process,frames_per_data=clip_size,resolution=resolution)
+    test_video_dataset = RGB(path_to_frames_validation_folder,static_val_videos,load_gt,process=process,frames_per_data=clip_size,resolution=resolution)
     test_data = DataLoader(test_video_dataset, batch_size=batch_size, drop_last=True)
 
 
-    test(test_data, model, device,path_to_save_saliency_maps)
+    test(test_data, model,load_gt, device,path_to_save_saliency_maps)
