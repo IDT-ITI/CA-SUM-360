472aa5c6e2c9feb559b96963a7c233fcf46991e4
ioankont <118340733+ioankont@users.noreply.github.com>
2023-12-08 16:30:44 +0200

    Update configs.py

--- 2D_Video_Production/configs.py
+++ 2D_Video_Production/configs.py
@@ -3,9 +3,9 @@ import argparse
 def args():
     parser = argparse.ArgumentParser(description='Args for 2D video production algorithm.')
     parser.add_argument('--saliency_maps_path', type=str,
-                        default=r"CA-SUM-360-main/data/VR-EyeTracking/saliency",
+                        default=r"data/VR-EyeTracking/saliency",
                         help='Directory path of saliency maps')
-    parser.add_argument('--frames_path', type=str, default=r"CA-SUM-360-main/data/VR-EyeTracking/frames",
+    parser.add_argument('--frames_folder_path', type=str, default=r"data/VR-EyeTracking/frames",
                         help='Directory path of frames')
 
     parser.add_argument('--intensity_value', type=int, default=150, help='Intensity value parameter')
@@ -13,7 +13,7 @@ def args():
     parser.add_argument('--spatial_distance', type=int, default=85, help='Spatial distance parameter')
     parser.add_argument('--fill_loss', type=int, default=60, help='Fill loss parameter')
 
-    parser.add_argument('--path_to_folder', type=str,
+    parser.add_argument('--fov_output_path', type=str,
                         default=r'outputs_fovs',
                         help='Path to save subvideos frames')
     parser.add_argument('--resolution', type=int, nargs=2, default=[1080, 1920], help='Resolution parameter')
