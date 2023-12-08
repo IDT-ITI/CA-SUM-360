2e6f46c65b53ab21832e164047d1afb5dea812a6
ioankont <118340733+ioankont@users.noreply.github.com>
2023-12-08 16:20:32 +0200

    Update configs.py

--- 2D_Video_Production/configs.py
+++ 2D_Video_Production/configs.py
@@ -3,18 +3,18 @@ import argparse
 def args():
     parser = argparse.ArgumentParser(description='Args for 2D video production algorithm.')
     parser.add_argument('--saliency_maps_path', type=str,
-                        default=r"D:\Program Files\IoanProjects\summary_data\saliency_sports",
+                        default=r"CA-SUM-360-main/data/VR-EyeTracking/saliency",
                         help='Directory path of saliency maps')
-    parser.add_argument('--frames_path', type=str, default=r"D:\Program Files\IoanProjects\360_Saliency_dataset_2018ECCV",
+    parser.add_argument('--frames_path', type=str, default=r"CA-SUM-360-main/data/VR-EyeTracking/frames",
                         help='Directory path of frames')
 
     parser.add_argument('--intensity_value', type=int, default=150, help='Intensity value parameter')
-    parser.add_argument('--dbscan_distance', type=float, default=600, help='DBSCAN distance parameter')
-    parser.add_argument('--spatial_distance', type=int, default=75, help='Spatial distance parameter')
-    parser.add_argument('--fill_loss', type=int, default=25, help='Fill loss parameter')
+    parser.add_argument('--dbscan_distance', type=float, default=1.5, help='DBSCAN distance parameter')
+    parser.add_argument('--spatial_distance', type=int, default=85, help='Spatial distance parameter')
+    parser.add_argument('--fill_loss', type=int, default=60, help='Fill loss parameter')
 
     parser.add_argument('--path_to_folder', type=str,
-                        default=r'CA-SUM-360/2D_Video_Production/outputs_fovs',
+                        default=r'outputs_fovs',
                         help='Path to save subvideos frames')
     parser.add_argument('--resolution', type=int, nargs=2, default=[1080, 1920], help='Resolution parameter')
     # Your code here
