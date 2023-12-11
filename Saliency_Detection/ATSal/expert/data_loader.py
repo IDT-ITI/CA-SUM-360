d679ff98355f86fcc8286cb48de94a350f1b3488
ioankont <118340733+ioankont@users.noreply.github.com>
2023-12-11 11:43:08 +0200

    Update data_loader.py

--- Saliency_Detection/ATSal/expert/data_loader.py
+++ Saliency_Detection/ATSal/expert/data_loader.py
@@ -14,7 +14,7 @@ from torch import nn
 
 class Multiexpert_dataset(Dataset):
 
-    def __init__(self, root_path,process,frames_per_data,resolution):
+    def __init__(self, root_path,video_names,process,frames_per_data,resolution):
 
         self.root_path = root_path
         self.process = process
@@ -23,7 +23,7 @@ class Multiexpert_dataset(Dataset):
         self.video_list = []
         self.dataset = []
         self.ImageNet_mean = [103.939, 116.779, 123.68]
-        video_names = os.listdir(root_path)
+        self.video_names = video_names
         if self.process=="train":
 
             for i, file in enumerate(video_names):
@@ -43,15 +43,12 @@ class Multiexpert_dataset(Dataset):
 
                 fpd = self.frames_per_data+20  #skip first 20 frames for train process because observers were exploring the 360 video from a fixed starting point
 
-                datast = []
-
                 for j in range(20, len(png_files),frames_per_data):
 
-                    #datast.append(png_files[j:fpd])
+
                     self.dataset.append(png_files[j:fpd])
                     fpd = fpd + frames_per_data
 
-                    #self.dataset.append(datast)
         else:
             for i, file in enumerate(video_names):
 
@@ -61,25 +58,18 @@ class Multiexpert_dataset(Dataset):
                 frames = os.listdir(frame_folder)
                 for j, fram in enumerate(frames):
 
-                    # if count % 3 == 0:
                     if fram.lower().endswith(('.png', '.jpg')):
                         png_files.append(frame_folder + "/" + fram)
-                    # count += 1
 
                 png_files = sorted(png_files)
-
                 fpd = self.frames_per_data
-
                 dat = []
 
                 for j in range(0, len(png_files), frames_per_data):
 
                     dat.append(png_files[j:fpd])
                     fpd = fpd + frames_per_data
-
                 self.dataset.append(dat)
-
-
 
 
     def __len__(self):
@@ -90,8 +80,8 @@ class Multiexpert_dataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        'Generates one sample of data'
-        #print(video_index)
+  
+
         frames = self.dataset[idx]
         #print(frames)
 
@@ -101,12 +91,7 @@ class Multiexpert_dataset(Dataset):
             data = []
             list_of_gt = []
 
-            #print(frames)
             for path_to_frame in frames:
-                #print(path_to_frame)
-
-                #for path_to_frame in paths_to_frame:
-                #print(path_to_frame)
 
                 path_to_gt = path_to_frame.replace('frames', 'saliency')
 
@@ -120,13 +105,9 @@ class Multiexpert_dataset(Dataset):
                 X = X.permute(2, 0, 1)  # swap channel dimensions
 
                 data.append(X.unsqueeze(0))
-                # Load and preprocess ground truth (saliency maps)
-
-
-
 
                 y = cv2.imread(path_to_gt, 0)  # Load as grayscale
-                # if self.resolution!=None:
+
                 y = cv2.resize(y, (self.resolution[1], self.resolution[0]), interpolation=cv2.INTER_AREA)
                 if y.max() != 0.0:
                     y = (y - np.min(y)) / (np.max(y) - np.min(y))
@@ -137,7 +118,6 @@ class Multiexpert_dataset(Dataset):
             gt_tensor = torch.cat(list_of_gt, 0)
 
             samples.append((data_tensor, gt_tensor))
-
 
         else:
 
@@ -150,7 +130,6 @@ class Multiexpert_dataset(Dataset):
 
 
                     path_to_gt = path_to_frame.replace('frames', 'saliency')
-
 
                     path_list.append(path_to_frame)
 
@@ -188,8 +167,3 @@ class Multiexpert_dataset(Dataset):
 
         return samples
 
-# video_name_list = dataset.video_names()
-
-
-# self.video_list.append(frame_files_sorted)
-# self.video_name_list.append(i)
